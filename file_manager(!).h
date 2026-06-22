2ced1d212d31bd29090ce90a8726b0215b4e224a
Dkrot-tech <danakrotovic@gmail.com>
2026-06-22 09:54:51 +0000

    Delete file_manager(!).h

--- file_manager(!).h
+++
@@ -1,9 +0,0 @@
-#ifndef FILE_MANAGER_H
-#define FILE_MANAGER_H
-
-#include <string>
-
-std::string readFile(const std::string& filename);
-void writeFile(const std::string& filename, const std::string& content);
-
-#endif
